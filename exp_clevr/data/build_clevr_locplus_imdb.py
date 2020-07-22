4500640a661b8e566073601dc9e8b989f406d394
Bryce Elizabeth Yahn <43454802+byahn2@users.noreply.github.com>
2020-07-22 15:28:36 -0400

    Update build_clevr_locplus_imdb.py

--- exp_clevr/data/build_clevr_locplus_imdb.py
+++ exp_clevr/data/build_clevr_locplus_imdb.py
@@ -15,6 +15,7 @@ def build_imdb(image_set):
     for n_q, q in enumerate(questions):
         if (n_q+1) % 10000 == 0:
             print('processing %d / %d' % (n_q+1, len(questions)))
+        ref_class = q['refexp_family_index']
         questionId = '%s_%s' % (image_set, q['refexp_index'])
         imageId = '%s_%s' % (image_set.replace('locplus_', ''), q['image_index'])
         question = q['refexp']
@@ -22,17 +23,26 @@ def build_imdb(image_set):
         iminfo = dict(questionId=questionId,
                       imageId=imageId,
                       question=question,
-                      image_name=image_name)
+                      image_name=image_name,
+                      ref_class=ref_class)
         # find boxes
         scene = scenes[q['image_index']]
         assert q['image_filename'] == scene['image_filename']
         obj_inds = q['program'][-1]['_output']
-        if len(obj_inds) > 1:  # skip refexps with more than one target object
-            continue
+        # BRYCE CODE
+        #if ref_class != 4:
+        #    continue
+        # bbox now is a list of bounding box coordinates for all the correct answers
         obj_boxes = scene['obj_bbox']
-        obj = scene['objects'][obj_inds[0]]
-        bbox = obj_boxes[str(obj['idx'])]
+        # obj_boxes is the set of bounding boxes for the scene
+        # obj_inds is the indeces of the target objects from the set of objects in the scene
+        bbox = []
+        # for each answer, find the box coordinates for the correct answer
+        for i in range(len(obj_inds)):
+            obj = scene['objects'][obj_inds[i]]
+            bbox.append(obj_boxes[str(obj['idx'])])
         iminfo['bbox'] = bbox
+        #BRYCE CODE
 
         imdb.append(iminfo)
     return imdb
